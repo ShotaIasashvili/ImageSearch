2af1fa886e1da42b1bbab8eb4d5d0dced7bbf1f2
ShotaIasashvili <shotaiasashvili@gmail.com>
2021-10-07 22:58:07 +0400

    Initial commit

---
+++ app/src/main/java/com/example/imagesearch/api/UnsplashResponse.kt
@@ -0,0 +1,5 @@
+package com.example.imagesearch.api
+
+import com.example.imagesearch.data.UnsplashPhoto
+
+data class UnsplashResponse(val results: List<UnsplashPhoto>)
---
+++ app/src/main/java/com/example/imagesearch/ui/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.imagesearch.ui
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.imagesearch.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_main) as NavHostFragment
+        navController = navHostFragment.findNavController()
+
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+}
